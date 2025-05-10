693426c922eef97ee7f4a7566cc4294e9fc1f450
Ryutera <kuya9823@gmail.com>
2025-05-10 21:11:35 +0100

    modal created

--- app/components/Book.tsx
+++ app/components/Book.tsx
@@ -2,14 +2,45 @@
 
 import Image from "next/image";
 import { BookType } from "../types/BookType";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 
 type BookProps = {
   book:BookType
 }
 
+
 // eslint-disable-next-line react/display-name
 const Book = ({ book }: BookProps) => {
+  const [showModal, setShowmodal] = useState(false)
+const {data: session} = useSession()
+const user = session?.user
+const router = useRouter()
+
+  const handleCancel = () =>{
+   
+    setShowmodal(false)
+    console.log(showModal)
+  }
+  
+  const handleOpen = () => {
+    setShowmodal(true)
+    console.log(showModal)
+
+  }
+
+  const handlePurchase = () => {
+    if (!user) {
+     setShowmodal(false)
+     router.push("/login")
+     //to login page
+    }else{
+      //to Stripe 
+    }
+  }
+  
   return (
     <>
       {/* アニメーションスタイル */}
@@ -29,8 +60,8 @@ const Book = ({ book }: BookProps) => {
         }
       `}</style>
 
-      <div className="flex flex-col items-center m-4">
-        <a className="cursor-pointer shadow-2xl duration-300 hover:translate-y-1 hover:shadow-none">
+      <div className="flex flex-col items-center m-4" >
+        <a onClick={handleOpen} className="cursor-pointer shadow-2xl duration-300 hover:translate-y-1 hover:shadow-none">
           <Image
             priority
             src={book.thumbnail.url}
@@ -46,17 +77,18 @@ const Book = ({ book }: BookProps) => {
           </div>
         </a>
 
-        {/* <div className="absolute top-0 left-0 right-0 bottom-0 bg-slate-900 bg-opacity-50 flex justify-center items-center modal">
+{showModal && (<div className="absolute top-0 left-0 right-0 bottom-0 bg-slate-200 bg-opacity-90 flex justify-center items-center modal">
           <div className="bg-white p-8 rounded-lg">
-            <h3 className="text-xl mb-4">本を購入しますか？</h3>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4">
-              購入する
+            <h3 className="text-xl mb-4">Do you buy the book？</h3>
+            <button onClick={handlePurchase} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4">
+              Buy
             </button>
-            <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
-              キャンセル
+            <button onClick={handleCancel} className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
+              Cancel
             </button>
           </div>
-        </div> */}
+        </div> )   }
+       
       </div>
     </>
   );
