3282b5911640630ec44b8b14d58737f383d9dc8d
Ryutera <kuya9823@gmail.com>
2025-05-16 23:11:33 +0100

    .

--- app/api/checkout/success/route.ts
+++ app/api/checkout/success/route.ts
@@ -7,7 +7,7 @@ import Stripe from "stripe"
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
-export async function POST (request:Request, response:Response){
+export async function POST (request:Request){
 
 
     const {sessionId} = await request.json()
